04a5005def351830e03eadb9a1c8d48e49f1bab4
AdnanM50 <a-19150@mangrove.edu.bd>
2025-08-12 10:42:32 +0600

    Update .gitignore to include early coverage files; refactor Home component in page.tsx by removing unused imports and commented code for cleaner structure.

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,5 +1,4 @@
-'use client'
-// import NemoFish from "@/components/site/landing/3d/nemoFIsh";
+
 import AquariumGuide from "@/components/site/landing/aquariumGuide";
 import BlogSection from "@/components/site/landing/blogSection";
 import FeaturedProducts from "@/components/site/landing/featurProduct";
@@ -7,27 +6,22 @@ import HeroSection from "@/components/site/landing/heroSection";
 import Newsletter from "@/components/site/landing/newsletter";
 import ProductCategories from "@/components/site/landing/ProductCategories";
 import Testimonials from "@/components/site/landing/testimonials";
-// import { useEffect, useState } from "react";
 
 
-export default function Home() {
-  // const [scrollY, setScrollY] = useState(0);
 
-  // useEffect(() => {
-  //   const handleScroll = () => setScrollY(window.scrollY);
-  //   window.addEventListener('scroll', handleScroll);
-  //   return () => window.removeEventListener('scroll', handleScroll);
-  // }, []);
+const Home = () => {
+
   return (
    <>
       <HeroSection />
       <ProductCategories />
-      <FeaturedProducts />
+      {/* <FeaturedProducts /> */}
       <Testimonials />
       <AquariumGuide />
       <BlogSection />
       <Newsletter />
-      {/* <NemoFish scrollY={scrollY} /> */}
+  
    </>
   );
 }
+export default Home;
